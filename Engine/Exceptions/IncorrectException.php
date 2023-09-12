74e3bf5dec2e032f4251d3ad3a88a96338d004be
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-09-12 20:03:22 +0300

    [0.0.2] Add skeleton.

---
+++ Engine/Exceptions/IncorrectException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Liloi\Atoms\Exceptions;
+
+class IncorrectException extends AtomsException
+{
+    /**
+     * Exception message.
+     *
+     * @var string
+     */
+    protected $defaultMessage = 'Incorrect RID exception.';
+
+    /**
+     * Exception code.
+     *
+     * @var int|string
+     */
+    protected $defaultCode = 0x103;
+}
