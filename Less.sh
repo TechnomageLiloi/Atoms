f01ebeae5f72e189dda979710a684514e02cdc6a
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-09-14 02:44:40 +0300

    [0.0.9] Add LESS to compiler list.

--- Less.sh
+++ Less.sh
@@ -2,3 +2,8 @@
 
 lessc ./Engine/Style.less ./Engine/Style.css
 lessc ./Engine/API/Style.less ./Engine/API/Style.css
+lessc ./Engine/API/Maps/Collection/Style.less ./Engine/API/Maps/Collection/Style.css
+lessc ./Engine/API/Maps/Edit/Style.less ./Engine/API/Maps/Edit/Style.css
+lessc ./Engine/API/Maps/Show/Style.less ./Engine/API/Maps/Show/Style.css
+lessc ./Engine/API/Repositories/Collection/Style.less ./Engine/API/Repositories/Collection/Style.css
+lessc ./Engine/API/Repositories/Edit/Style.less ./Engine/API/Repositories/Edit/Style.css
